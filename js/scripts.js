6413e416227f59b5333d78cc380739b6d08dbca7
Emilie Thoreson and Saul Artiga <emilie+saul@>
2017-06-05 14:59:52 -0700

    add alphabetized results that are capitalized

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,20 @@
 $(function(){
   var masterArray = [];
-  $("button#submit").click(function(event) {
-    event.preventDefault();
 
-    // for (x = 0; x < 1; x++){
-    //
-    // }
-
-
+  $("button#add").click(function(event) {
     var newelement = $("#new-item").val();
     masterArray.push(newelement);
     masterArray.sort();
-    // var newArray = masterArray.map(function(task) {
-    //   task.toUpperCase();
-    // });
-
-    $("#printed-list").append("<li>" + newelement + "</li>");
-    $("#printed-list").show();
-    console.log(masterArray);
-
   });
 
-  $("button#remove").click(function(event) {
+  $("button#submit").click(function(event) {
     event.preventDefault();
-    masterArray.pop();
-    $("ul li:first-child").empty();
-    console.log(masterArray);
-    $("#printed-list").show();
+    $("#printed-list").empty()
+    var upperArray = [];
+    masterArray.forEach(function(task){
+      upperArray.push(task.toUpperCase());
+      $("#printed-list").append("<li class='testcolor'>" + task.toUpperCase() + "</li>");
+      $("#printed-list").show();
+    });
   });
-})
+});
