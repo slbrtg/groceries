ed964d096a01739d170d04dbb8c862c23365055e
Emilie Thoreson and Saul Artiga <saul@>
2017-06-05 13:43:31 -0700

    populate html and css, add adding-to-list logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-  
-});
+$(function(){
+  var masterArray = [];
+  $("button#submit").click(function(event) {
+    event.preventDefault();
+    var newelement = $("#new-item").val();
+    masterArray.push($("#new-item").val());
+    $("#printed-list").append("<li>" + newelement + "</li>")
+    $("#printed-list").show();
+    console.log(masterArray);
+  });
+})
