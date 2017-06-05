6356ba67ba0114875672d1b630a8aa61cc7f66d1
Emilie Thoreson and Saul Artiga <emilie+saul@>
2017-06-05 14:28:19 -0700

    work on js backend

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,30 @@ $(function(){
   var masterArray = [];
   $("button#submit").click(function(event) {
     event.preventDefault();
+
+    // for (x = 0; x < 1; x++){
+    //
+    // }
+
+
     var newelement = $("#new-item").val();
-    masterArray.push($("#new-item").val());
-    $("#printed-list").append("<li>" + newelement + "</li>")
+    masterArray.push(newelement);
+    masterArray.sort();
+    // var newArray = masterArray.map(function(task) {
+    //   task.toUpperCase();
+    // });
+
+    $("#printed-list").append("<li>" + newelement + "</li>");
     $("#printed-list").show();
     console.log(masterArray);
+
+  });
+
+  $("button#remove").click(function(event) {
+    event.preventDefault();
+    masterArray.pop();
+    $("ul li:first-child").empty();
+    console.log(masterArray);
+    $("#printed-list").show();
   });
 })
