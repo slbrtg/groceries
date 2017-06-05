05a284c2dcfecd6202b876ac8c765e591d22fb2f
Emilie Thoreson and Saul Artiga <emilie+saul@>
2017-06-05 15:33:46 -0700

    add styles and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,13 @@
 $(function(){
   var masterArray = [];
-
-  $("button#add").click(function(event) {
+  $("button#submit").click(function(event) {
+    event.preventDefault();
+    $("#printed-list").empty();
     var newelement = $("#new-item").val();
     masterArray.push(newelement);
     masterArray.sort();
-  });
-
-  $("button#submit").click(function(event) {
-    event.preventDefault();
-    $("#printed-list").empty()
-    var upperArray = [];
     masterArray.forEach(function(task){
-      upperArray.push(task.toUpperCase());
-      $("#printed-list").append("<li class='testcolor'>" + task.toUpperCase() + "</li>");
+      $("#printed-list").append("<li class='testcolor'>" + task.toUpperCase() + "<button type='button' class='btn btn-danger btn-lg active' id='remove'>Remove</button>"+"</li>");
       $("#printed-list").show();
     });
   });
