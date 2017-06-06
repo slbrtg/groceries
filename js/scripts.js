bd28e07f899d8605ec315ea140cbed06470ed271
Saul Artiga <slbrtg@gmail.com>
2017-06-05 20:40:02 -0700

    redevelop js back end and switch for loops for document.on functions and submit btn actions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-$(function(){
-  var masterArray = [];
-  $("button#submit").click(function(event) {
-    event.preventDefault();
-    $("#printed-list").empty();
-    var newelement = $("#new-item").val();
-    masterArray.push(newelement);
-    masterArray.sort();
-    masterArray.forEach(function(task){
-      $("#printed-list").append("<li class='testcolor'>" + task.toUpperCase() + "<button type='button' class='btn btn-danger btn-lg active' id='remove'>Remove</button>"+"</li>");
-      $("#printed-list").show();
-    });
-  });
+var total = 0;
+
+$(document).on("submit", "form#grocery-list", function(){
+  event.preventDefault();
+  var newElement = $("#new-item").val();
+  $("#printed-list").append("<li class='item'>"+ newElement + "<button type='submit' class='btn btn-danger btn-lg active remove-btn' >Remove</button></li>");
+  $("#printed-list").show();
+});
+
+$(document).on("click", ".remove-btn",function() {
+  event.preventDefault();
+  $(this).parent().empty();
 });
